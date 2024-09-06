1717c3389f9368d3109ccae8b44bf251c5e44464
Justin Maronde <myemailisjustin@me.com>
2024-09-06 10:20:27 -0400

    Initial Commit

---
+++ SwiftUIAndCombine/SwiftUIAndCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAndCombineApp.swift
+//  SwiftUIAndCombine
+//
+//  Created by Justin Maronde on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAndCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
